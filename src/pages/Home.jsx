91135bd0ccbb44b8801dfc84a7298ef676d1ee6a
Elif Yuzer <elifyuzer12@gmail.com>
2026-07-28 12:54:23 +0200

    update: search updated with the url query as `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${searchTerm}` and tracked by useState and add handleSearch function
    :qa

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,28 +1,38 @@
 import React, { useState, useContext } from "react";
 import { MovieContext } from "../context/MovieContext";
-import { useNavigate } from "react-router-dom";
 import MovieCard from "../components/MovieCard";
 
 const Home = () => {
-  const navigate = useNavigate();
-  const { films } = useContext(MovieContext);
+ 
+  const { films,getFilmData,api_key,url } = useContext(MovieContext);
   const [searchTerm, setSearchTerm] = useState("");
-  const filteredFilms = films.filter((film) =>
-    film.title.toLowerCase().includes(searchTerm.toLowerCase()),
-  );
+
+  const handleSearch = (e) => {
+    e.preventDefault();
+    if (!searchTerm.trim()) {
+      getFilmData(url); // boşsa listeyi sıfırla
+      return;
+    }
+    getFilmData(
+      `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${searchTerm}`,
+    );
+  };
 
   return (
     <div className="px-4 py-8">
-      <div className="mb-8 flex gap-2 justify-center shadow-sm bg-base-100 rounded-lg p-4">
-        <input
+      <div className="mb-8 flex  gap-2 justify-center shadow-sm bg-base-100 rounded-lg p-4">
+       <form onSubmit={handleSearch}
+       className="flex gap-2"
+       > <input
           type="text"
           placeholder="Search a movie..."
           className="input input-bordered "
           onChange={(e) => setSearchTerm(e.target.value)}
         />
-        <button className="btn btn-outline">Search</button>
+        <button type="submit" className="btn btn-outline rounded-xl">Search</button>
+        </form>
       </div>
-      <MovieCard filteredFilms={filteredFilms} />
+      <MovieCard />
     </div>
   );
 };
